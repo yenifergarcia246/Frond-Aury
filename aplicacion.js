778eb34f63a63d5f9364b0fa2b7dcd5024ffa62e
yenifergarcia246 <yenifergarciamoreno8@gmail.com>
2025-11-19 14:53:53 -1200

    Actualización final del proyecto Aury, funcionalidades listas

--- aplicacion.js
+++ aplicacion.js
@@ -9,3 +9,20 @@ btnRegistro.addEventListener("click", () => {
 btnInicio.addEventListener("click", () => {
   contenedor.classList.remove("modo-registro");
 });
+
+
+// ESta parte es sobre la conexion que hice con el firebase
+
+// Esperar a que el contenido cargue
+document.addEventListener("DOMContentLoaded", () => {
+  // Seleccionar todos los íconos de Google (login y registro)
+  const botonesGoogle = document.querySelectorAll(".icono-red .fa-google");
+
+  // Agregar evento a cada uno
+  botonesGoogle.forEach((boton) => {
+    boton.addEventListener("click", (e) => {
+      e.preventDefault();
+      alert("🔒 Próximamente podrás iniciar sesión con Google 😄");
+    });
+  });
+});
